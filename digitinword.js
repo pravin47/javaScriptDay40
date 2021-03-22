1e4691ef93b690d40b912aa0b06110a8b9543c37
pravin jagannath bagul <77392453+pravin47@users.noreply.github.com>
2021-03-22 23:54:40 +0530

    convertdigitinwordusingIFElSE

---
+++ digitinword.js
@@ -0,0 +1,23 @@
+const prompt = require('prompt-sync')({sigint:true});
+let number = prompt("Enter The Number Between 1 to 9");
+if(number == 1){
+    console.log('one');
+}else if(number == 2){
+    console.log('two');
+}else if(number == 3){
+    console.log('three');
+}else if(number == 4){
+    console.log('four');
+}else if(number == 5){
+    console.log('five');
+}else if(number == 6){
+    console.log('six');
+}else if(number == 7){
+    console.log('seven');
+}else if(number == 8){
+    console.log('eight');
+}else if(number == 9){
+    console.log('nine');
+}else{
+    console.log('invalid entry');
+}
